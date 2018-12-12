4f74d6370aed6e4d1938650e720c1e6124e7bc71
larsbratholm <larsbratholm@gmail.com>
2018-12-12 16:10:43 +0000

    Updated for paper

--- main.py
+++ main.py
@@ -11,21 +11,10 @@ from osprey.config import Config
 from osprey.trials import Trial
 from osprey.strategies import GP
 
-def from_gp(result, searchspace):
-
-    # Note that GP only deals with float-valued variables, so we have
-    # a transform step on either side, where int and enum valued variables
-    # are transformed before calling gp, and then the result suggested by
-    # GP needs to be reverse-transformed.
-    out = {}
-    for gpvalue, var in zip(result, searchspace):
-        out[var.name] = var.point_from_gp(float(gpvalue))
-
-    return out
-
 def get_data(history, searchspace):
     """
-    Get features, mean scores, variance of scores and elapsed time in seconds
+    Get features, scores and elapsed time in seconds, as well as parameters used for
+    transforming the variable space.
     """
     X = []
     Y = []
@@ -51,8 +40,11 @@ def get_data(history, searchspace):
         else:
             raise RuntimeError('unrecognized status: %s' % status)
 
+    # Transform the parameters by subtracting means and transform to log scale
     transformed_scores = np.log(- np.asarray(all_scores))
-    transformed_time = np.log(TY)
+    n_folds = transformed_scores.shape[1]
+    # Divide by n_folds as time is for all folds
+    transformed_time = np.log(np.asarray(TY)/n_folds)
     transform_params = (np.mean(transformed_scores), np.std(transformed_scores, ddof=1),
                         np.mean(transformed_time), np.std(transformed_time, ddof=1))
     transformed_scores -= transform_params[0]
@@ -61,19 +53,23 @@ def get_data(history, searchspace):
     transformed_time /= transform_params[3]
 
 
+    TX = X.copy()
+    TY = transformed_time
+
     # Include all cv folds in the scoring instead of using averages
     Y = transformed_scores.ravel()
-    TY = list(itertools.chain.from_iterable(itertools.repeat(x,3) for x in transformed_time))
-    X = list(itertools.chain.from_iterable(itertools.repeat(x,3) for x in X))
+    X = list(itertools.chain.from_iterable(itertools.repeat(x,n_folds) for x in X))
+
 
     return (np.array(X).reshape(-1, searchspace.n_dims),
             np.array(Y).reshape(-1, 1),
+            np.array(TX).reshape(-1, searchspace.n_dims),
             np.array(TY).reshape(-1,1),
             transform_params)
 
 def data_from_config(config_file):
     """
-    Returns features, scores, score variance, elapsed time in seconds
+    Returns features, scores, elapsed time in seconds etc.
     and searchspace from a config file
     """
     config = Config(config_file)
@@ -90,19 +86,16 @@ def score_mae(y, y_pred):
 
 def fit_gp(X, Y):
     """
-    Fit GP
+    Fit GP with three different kernels. The kernel with the lowest AICc is used.
     """
-    kernels =  [GPy.kern.RBF(input_dim=X.shape[1],ARD=True) + GPy.kern.Bias(X.shape[1]),
-                GPy.kern.RBF(input_dim=X.shape[1],ARD=True),
-                GPy.kern.Matern52(input_dim=X.shape[1],ARD=True) + GPy.kern.Bias(X.shape[1]),
+    # White kernel is effectively included in the kernels.
+    # There's no need for a bias since we have subtracted it in the transform
+    kernels =  [GPy.kern.RBF(input_dim=X.shape[1],ARD=True),
                 GPy.kern.Matern52(input_dim=X.shape[1],ARD=True),
-                GPy.kern.Matern32(input_dim=X.shape[1],ARD=True) + GPy.kern.Bias(X.shape[1]),
-                GPy.kern.Matern32(input_dim=X.shape[1],ARD=True),
-                GPy.kern.Exponential(input_dim=X.shape[1],ARD=True) + GPy.kern.Bias(X.shape[1]),
-                GPy.kern.Exponential(input_dim=X.shape[1],ARD=True)]
+                GPy.kern.Matern32(input_dim=X.shape[1],ARD=True)]
+
     models = []
     for kernel in kernels:
-
         #if V is not None:
         #    kernel += GPy.kern.WhiteHeteroscedastic(X.shape[1],X.shape[0])
 
@@ -131,7 +124,7 @@ def ic(model,method='bic'):
         return 2 * k - 2 * ll + (2 * k**2 + 2 * k) / (n - k - 1)
 
 def get_gp_best(score_model, time_model, transform_params, freeze_idx=None, freeze_value=None,
-        nsamples = 10000, nopt = 100, time_limit = 36):
+        nsamples = 10000, nopt = 100, time_limit = 8):
     # Objective function
     def z(x):
         if freeze_idx is not None:
@@ -185,8 +178,15 @@ def get_gp_best(score_model, time_model, transform_params, freeze_idx=None, free
 
     time_mask = np.exp(Z.ravel() * transform_params[3] + transform_params[2]) / 3600 < time_limit
 
-    idx = np.argpartition(Y.ravel()[time_mask], nopt)[:nopt]
-    X = X[time_mask][idx]
+    # Update nopt to not be larger than the number of masked values
+    n = min(nopt, time_mask.sum()-1)
+    if n > 0:
+        idx = np.argpartition(Y.ravel()[time_mask], n)[:n]
+        X = X[time_mask][idx]
+    else:
+        # Ignore the time_mask if no solution is found
+        idx = np.argpartition(Y.ravel(), nopt)[:nopt]
+        X = X[idx]
 
     # Optimize using these points at starting positions
     topx, topy = [], []
@@ -209,22 +209,33 @@ def get_gp_best(score_model, time_model, transform_params, freeze_idx=None, free
 
     return x, topy[best_idx]
 
-def plot_single(score_model, time_model, transform_params):
+def plot_single(score_model, time_model, transform_params, time_limit=8):
+    """
+    Plot how each parameter affects the score. All variables
+    except one is optimized.
+    """
 
     nfeatures = score_model.input_dim
 
 
+    all_X = []
+    all_Y = []
+    all_names = []
     for i in range(nfeatures):
         X, Y = [], []
         rang = np.linspace(0,1,20)
         for value in rang:
             x, y = get_gp_best(score_model, time_model, transform_params,
-                    freeze_idx=i, freeze_value=value, nsamples = 100, nopt = 10)
+                    freeze_idx=i, freeze_value=value, nsamples = 100, nopt = 10,
+                    time_limit=time_limit)
 
             y = np.exp(float(y)*transform_params[1] + transform_params[0])
+            z, _ = time_model.predict(x.reshape(1,-1))
+
+            actual_time = np.exp(float(z) * transform_params[3] + transform_params[2])
             x = list(searchspace.point_from_gp(x).values())[i]
             X.append(x)
-            Y.append(y)
+            Y.append(actual_time/3600) # change
 
         xname = list(searchspace.point_from_gp(np.zeros(nfeatures)).keys())[i]
 
@@ -232,24 +243,47 @@ def plot_single(score_model, time_model, transform_params):
             for j, x in enumerate(X):
                 f.write("%s %s" % (str(x), str(Y[j])))
 
+        all_X.append(X)
+        all_Y.append(Y)
+        all_names.append(xname)
+
+
+    y_min = np.min(np.asarray(all_Y)) * 0.9
+    y_max = np.max(np.asarray(all_Y)) * 1.1
+    for i in range(nfeatures):
+        xname = all_names[i]
+        X = all_X[i]
+        Y = all_Y[i]
         plt.title(xname)
+        plt.ticklabel_format(useOffset=False)
         plt.loglog(X,Y)
-        plt.savefig("%s.png" % xname)
+        plt.ylim(y_min,y_max)
+        #plt.tight_layout()
+        plt.savefig("%s.png" % xname, dpi=200)
         plt.clf()
 
 
 if __name__ == "__main__":
     config_file = sys.argv[1]
-    X, Y, TY, transform_params, searchspace = data_from_config(config_file)
+    # Parse the config file
+    X, Y, TX, TY, transform_params, searchspace = data_from_config(config_file)
+    #n = 75
+    #X = X[:3*n]
+    #Y = Y[:3*n]
+    #TX = TX[:n]
+    #TY = TY[:n]
+    # Fit score and time model
     score_model = fit_gp(X,Y)
-    time_model = fit_gp(X,TY)
+    time_model = fit_gp(TX,TY)
 
-
+    # Get the hyper-parameters that yields the best estimate.
+    # "Best" is in this case the fitted mean + 1 stdev.
+    # A constraint of a maximum expected fitting time of 8 hours is used
     x, y = get_gp_best(score_model, time_model, transform_params,
-            nsamples=1000, nopt=100, time_limit=24)
+            nsamples=1000, nopt=100, time_limit=8)
+    # Convert back to human readable format for mae and hyper-params
     y = np.exp(float(y)*transform_params[1] + transform_params[0])
     x = searchspace.point_from_gp(x)
     print(y)
     print(x)
-    quit()
-    plot_single(score_model, time_model, transform_params)
+    #plot_single(score_model, time_model, transform_params, time_limit=8)
